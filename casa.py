0942ac2f7dee748ff29c40d5e315a78072c665fe
Bossk <jb.panecatl@gmail.com>
2021-06-28 20:15:24 -0500

    Modifications to calculator, but left as was originally.

--- casa.py
+++ casa.py
@@ -1,5 +1,4 @@
 # Program make a simple calculator
-
 # This function adds two numbers
 def add(x, y):
     return x + y
@@ -18,20 +17,20 @@ def divide(x, y):
 
 
 print("Select operation.")
-print("1.Add")
-print("2.Subtract")
-print("3.Multiply")
-print("4.Divide")
+print("1. Add")
+print("2. Subtract")
+print("3. Multiply")
+print("4. Divide")
 
 while True:
     # Take input from the user
-    choice = input("Enter choice(1/2/3/4): ")
+    choice = input("Enter choice ( 1 / 2 / 3 / 4 ): ")
 
     # Check if choice is one of the four options
     if choice in ('1', '2', '3', '4'):
         num1 = float(input("Enter first number: "))
         num2 = float(input("Enter second number: "))
-
+        
         if choice == '1':
             print(num1, "+", num2, "=", add(num1, num2))
 
@@ -43,6 +42,8 @@ while True:
 
         elif choice == '4':
             print(num1, "/", num2, "=", divide(num1, num2))
+
         break
+
     else:
         print("Invalid Input")
