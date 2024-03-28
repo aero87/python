d2c8f5702946c86af8e3ae628bf2ddeacba22281
Daniel Bahena <a01424506@tec.mx>
2024-03-28 14:41:49 -0600

    Program to find the largest number in a list, with input by user.

---
+++ largest_element_list.py
@@ -0,0 +1,21 @@
+# Python program to find lasgest element in a list
+
+def find_largest(numbers):
+    if not numbers:
+        return None
+    largest = numbers[0]
+    for num in numbers:
+        if num > largest:
+            largest = num
+    return largest
+
+nums = list(map(int, input("Insert a series of numbers separated by a space: ").split()))
+
+largest_num = find_largest(nums)
+if largest_num is not None:
+    print(f"The largest number is {largest_num}")
+    
+else:
+    print("The list is empty.")
+    
+# print(f"The largest numer is {largest_num}")
